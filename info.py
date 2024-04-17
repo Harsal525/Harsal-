c859c6d99b05ef00f7df48db925bb1640367f723
Harsal <149051859+Harsal525@users.noreply.github.com>
2024-04-17 13:51:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,7 +55,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False)) # True if you want n
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "")
+DATABASE_NAME = environ.get('DATABASE_NAME', "cluster")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Verify
